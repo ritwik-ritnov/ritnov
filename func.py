a8b0dca0be1556d8b66c74b62e7912ef3f8d04b4
ritwik-walter <77221327+ritwik-walter@users.noreply.github.com>
2023-01-29 17:14:10 +0530

    Update func.py

--- func.py
+++ func.py
@@ -1,3 +1,4 @@
+#check leap year
 def is_leap(year):
     if year%100==0:
         if year%400==0:
@@ -9,3 +10,11 @@ def is_leap(year):
     else:
         leap = False
     return leap
+
+#if n=5, it will print 12345.....n
+def tex(x):
+        a = 1
+        while a<x:
+            print(a,end="")
+            a = a+1
+        return a
